88dda347f07534eb206c2d73d17a38f63e22cfd8
Armando Rios <armandorios.dev@gmail.com>
2025-02-04 03:46:07 -0800

    feat: add transition efect to change sections

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,17 +1,27 @@
 import heroImg from "../img/hero.jpg"
 
 export function home() {
-  content.innerHTML = ""
   const image = document.createElement("img")
   const title = document.createElement("h2")
   const subtitle = document.createElement("h3")
+
+  title.classList.add("fade-out");
+  subtitle.classList.add("fade-out");
+  content.innerHTML = ""
   image.src = heroImg
   image.classList.add("content-bg")
-  title.textContent = "Michelin Star Experience"
-  subtitle.textContent = "An unparalleled fine dining experience"
-  title.classList.add("hero-title")
-  subtitle.classList.add("hero-subtitle")
   content.append(image)
-  content.append(title)
-  content.append(subtitle)
+
+  setTimeout(() => {
+    title.textContent = "Michelin Star Experience"
+    subtitle.textContent = "An unparalleled fine dining experience"
+    title.classList.add("hero-title")
+    subtitle.classList.add("hero-subtitle")
+    content.append(title)
+    content.append(subtitle)
+    title.classList.remove("fade-out"); // Quita la animación de salida
+    title.classList.add("fade-in"); // Aplica la animación de entrada
+    subtitle.classList.remove("fade-out"); // Quita la animación de salida
+    subtitle.classList.add("fade-in"); // Aplica la animación de entrada
+  }, 300)
 }
--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,15 +1,23 @@
 import heroImg from "../img/menu.webp"
 
 export function menu() {
-  content.innerHTML = ""
   const image = document.createElement("img")
   const title = document.createElement("h2")
   const subtitle = document.createElement("h3")
+  title.classList.add("fade-out");
+  subtitle.classList.add("fade-out");
+  content.innerHTML = ""
   image.src = heroImg
   image.classList.add("content-bg")
-  title.textContent = "Our Signature Dishes"
-  subtitle.textContent = "Indulge in curated selection of gourmet delights"
   content.append(image)
-  content.append(title)
-  content.append(subtitle)
+  setTimeout(() => {
+    title.textContent = "Our Signature Dishes"
+    subtitle.textContent = "Indulge in curated selection of gourmet delights"
+    content.append(title)
+    content.append(subtitle)
+    title.classList.remove("fade-out"); // Quita la animación de salida
+    title.classList.add("fade-in"); // Aplica la animación de entrada
+    subtitle.classList.remove("fade-out"); // Quita la animación de salida
+    subtitle.classList.add("fade-in"); // Aplica la animación de entrada
+  }, 300)
 }
